94c9e8b346264110eca001cb87e64d7bf7be39b9
Lukáš Dočkal <lukas.dockal@olc.cz>
2018-01-06 23:48:44 +0100

    css - board

--- board/forms.py
+++ board/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class BoardNewPost(forms.Form):
-    text = forms.CharField(widget=forms.Textarea)
+    text = forms.CharField(required=True, label="", widget=forms.Textarea(attrs={'rows': 8, 'cols': 160}))
