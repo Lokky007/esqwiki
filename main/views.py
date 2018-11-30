2d72565644ac28b41bb3708a6f89ace81e4c6d6e
Lukáš Dočkal <lukas.dockal@olc.cz>
2018-11-30 23:36:03 +0100

    no image

--- main/views.py
+++ main/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from wiki.models import WikiCraftProductType
+from wiki.models import WikiCraftProduct,WikiItem
+
 
 # Create your views here.
 def index(request):
-    records = WikiCraftProductType.objects.filter().order_by('-x_created')
+    records_items = WikiItem.objects.filter().order_by('-x_created')
+    records_products = WikiCraftProduct.objects.filter().order_by('-x_created')
 
     list_news = []
-    for row in records:
+    for row in records_items:
+        list_news.append(row)
+
+    for row in records_products:
         list_news.append(row)
 
     return render(request, 'main_base.html', {'newest_records': list_news})
