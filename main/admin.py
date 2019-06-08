4fbbe28f64c8623586a6f90cde8079d556417204
Lukáš Dočkal <lukas.dockal@olc.cz>
2019-06-08 22:18:22 +0200

    Tasks - admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from main.models import ProjectTasks
+
+
+class ProjectTasksAdmin(admin.ModelAdmin):
+    model = ProjectTasks
+
+    list_display = ("name", "complete_percentage")
+
+admin.site.register(ProjectTasks, ProjectTasksAdmin)
