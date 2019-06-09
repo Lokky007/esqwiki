0e8dd873b0dd7ee00ccc47e9281ed8dd45fe12fd
Lukáš Dočkal <lukas.dockal@olc.cz>
2019-06-09 21:11:25 +0200

    forum - migration

--- forum/models.py
+++ forum/models.py
@@ -1,5 +1,48 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class AccessRight(models.Model):
+    id_access_right = models.AutoField(primary_key=True)
+    access_name = models.CharField(blank=False, null=False, max_length=256)
+    x_created = models.DateTimeField(auto_now_add=True)
+    x_modified = models.DateTimeField(auto_now=True)
+    x_user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+
+class Category(models.Model):
+    id_category = models.AutoField(primary_key=True)
+    title = models.CharField(blank=False, null=False, max_length=256)
+    description = models.CharField(max_length=2048)
+    show = models.BooleanField(default=True)
+    access_right = models.ForeignKey(AccessRight, blank=True, null=True)
+    x_created = models.DateTimeField(auto_now_add=True)
+    x_modified = models.DateTimeField(auto_now=True)
+    x_user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+
+class Topic(models.Model):
+    id_topic = models.AutoField(primary_key=True)
+    name = models.CharField(blank=False, null=False, max_length=256)
+    text = models.CharField(blank=False, null=False, max_length=2048)
+    deleted = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, blank=True, null=True)
+    x_created = models.DateTimeField(auto_now_add=True)
+    x_modified = models.DateTimeField(auto_now=True)
+    x_user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+
+class Answer(models.Model):
+    id_answer = models.AutoField(primary_key=True)
+    text = models.CharField(blank=False, null=False, max_length=2048)
+    like = models.IntegerField(default=0)
+    dislike = models.IntegerField(default=0)
+    deleted = models.BooleanField(default=False)
+    topic = models.ForeignKey(Topic)
+    x_created = models.DateTimeField(auto_now_add=True)
+    x_modified = models.DateTimeField(auto_now=True)
+    x_user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
